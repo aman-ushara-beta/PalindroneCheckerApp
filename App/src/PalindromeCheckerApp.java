2163e22660629940212cae1fa580c7cc072d18bf
aman-ushara-beta <aman.ushara22@gmail.com>
2026-03-13 04:02:28 +0530

    / test commit

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+public class PalindromeCheckerApp
+{
+
+    /**
+     * Application entry point.
+     * * This is the first method executed by the JVM
+     * when the program starts.
+     * * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        // Displaying the Welcome Message
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        
+        // Displaying the Application Version
+        System.out.println("Version : 1.0");
+        
+        // Displaying Initialization Status
+        System.out.println("System initialized successfully.");
+    }
+}
--- App/src/PalindroneCheckerApp.java
+++
@@ -1,7 +0,0 @@
-public class PalindroneCheckerApp
-{
-    public static void main(String[] args)
-    {
-
-    }
-}
